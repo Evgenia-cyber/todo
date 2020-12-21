bb33fe5bfdf2fb64c9b24ce9723985b5f3d4c087
Evgenia Kurineva <kurineva85@mail.ru>
2020-12-21 19:05:53 +0300

    Version 2.01: fixed custom hook to reset the input value

--- src/components/InputAddTodo.js
+++ src/components/InputAddTodo.js
@@ -2,31 +2,31 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function useInputValue(defaultValue = "") {
-   const [value, setValue] = useState(defaultValue);
-   return {
-     value: value,
-     onChange: (event) => setValue(event.target.value),
-   };
- }
+  const [value, setValue] = useState(defaultValue);
+  return {
+    inputObjectFromUseInputValue: {
+      value: value,
+      onChange: (event) => setValue(event.target.value),
+    },
+    clear: () => setValue(""),
+    value: () => value,
+  };
+}
 
 function InputAddTodo({ onCreate }) {
-  let input=useInputValue('');
+  const input = useInputValue("");
 
   function submitHandler(event) {
     event.preventDefault();
-    if (input.value.trim()) {
-      onCreate(input.value);
-      input='';
+    if (input.value().trim()) {
+      onCreate(input.value());
+      input.clear();
     }
   }
-  
+
   return (
     <form className="form" onSubmit={submitHandler}>
-      <input
-        type="text"
-        placeholder="Enter new todo"
-       {...input}
-      />
+      <input type="text" placeholder="Enter new todo" {...input.inputObjectFromUseInputValue} />
       <button type="submit">Add todo</button>
     </form>
   );
